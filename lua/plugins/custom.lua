72f9933153fdcf6a6be19854d9d82d4fabd90c32
Paolo Banez <paolobanez@gmail.com>
2024-04-15 00:18:41 +0800

    update colorscheme

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -17,10 +17,11 @@ return {
       },
     },
   },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-nightasty",
+      colorscheme = "moonfly",
     },
   },
   {
